20df590bdca6272553f8ad0ebbe27cbe8ffd22f2
shioriii <ma.shi842@gmail.com>
2020-05-05 21:53:54 +0900

    add css file

--- kanban/forms.py
+++ kanban/forms.py
@@ -6,12 +6,22 @@ from .models import List, Card
 
 class UserForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = User
         fields = ("username", "last_name", "first_name", "email",)
 
 
 class ListForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(ListForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = List
@@ -20,6 +30,11 @@ class ListForm(forms.ModelForm):
 
 class CardForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super(CardForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Card
         fields = ("title", "description", "list")
@@ -27,6 +42,23 @@ class CardForm(forms.ModelForm):
 
 class CardCreateFromHomeForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super(CardCreateFromHomeForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Card
         fields = ("title", "description",)
+
+
+class CardUpdateForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(CardUpdateForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        model = Card
+        fields = ("title", "description", "list")
