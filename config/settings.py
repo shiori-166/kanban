94d6b06ff84e26b4c18a76e4e0e0f8d4c07d4a14
shioriii <ma.shi842@gmail.com>
2020-05-06 11:45:22 +0900

    make it better

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '+5ww$keemc2h$57t6cg_r=)i%q$#9!s^mz5b4u12h+v4l*u#m&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["techpit-kanban-2020.herokuapp.com"]
+ALLOWED_HOSTS = ["https:techpit-kanban-2020.herokuapp.com"]
 
 
 # Application definition
