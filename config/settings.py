55d94d3284138cc19f3c832bc27b13f0aa0c25b8
shioriii <ma.shi842@gmail.com>
2020-05-06 18:32:08 +0900

    fix settings

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -148,7 +149,6 @@ except ImportError:
     pass
 
 if not DEBUG:
-    import django_heroku
     django_heroku.settings(locals())
 
 DEBUG = True
